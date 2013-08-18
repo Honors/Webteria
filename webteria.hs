00fadad3f15e000465c531c469b8cbf95365ddfd
Matt Neary <neary.matt@gmail.com>
2013-08-17 21:21:13 -0400

    beginning of permutation

--- webteria.hs
+++ webteria.hs
@@ -5,4 +5,7 @@ isOrigin center rest = (foldl (+) 0 (map snd rest)) == (snd center)
 complement x set = filter (x /=) set
 hasOrigin atoms = foldl (||) False (map (\c -> isOrigin c (complement c atoms)) atoms)
 groupElms set = map (\ss -> (head ss, length ss)) (group (sort (map fst set)))
-main = putStrLn $ show $ groupElms [("C", 4), ("O", 2), ("O", 2)]
+enumerateRows rows = [[(fst r, i) | i <- [0..l]] | r <- rows, let l = (snd r)]
+-- | TODO: decisionProduct xs ys = 
+subMols set = groups where groups = (enumerateRows (groupElms set))
+main = putStrLn $ show $ enumerateRows $ groupElms [("C", 4), ("O", 2), ("O", 2)]
